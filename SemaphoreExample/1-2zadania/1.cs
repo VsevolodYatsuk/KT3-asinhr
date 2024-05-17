dcc9f7aa7fadb344c8ca7fa8fe8b55dfdf10bb59
Всеволод <sevateti@yandex.com>
2024-05-17 21:05:50 +0300

    1-2zad+

---
+++ SemaphoreExample/1-2zadania/1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int resourceCount = 3; // количество доступных ресурсов
+        SemaphoreSlim semaphore = new SemaphoreSlim(resourceCount);
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int threadNumber = i;
+            new Thread(() =>
+            {
+                Console.WriteLine($"Поток {threadNumber} ожидает доступа к ресурсу.");
+                semaphore.Wait();
+                try
+                {
+                    Console.WriteLine($"Поток {threadNumber} получил доступ к ресурсу.");
+                    Thread.Sleep(1000); // имитация работы с ресурсом
+                }
+                finally
+                {
+                    Console.WriteLine($"Поток {threadNumber} освобождает ресурс.");
+                    semaphore.Release();
+                }
+            }).Start();
+        }
+    }
+}
---
+++ SemaphoreExample/1-2zadania/2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        int resourceCount = 3; 
+        SemaphoreSlim semaphore = new SemaphoreSlim(resourceCount);
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int threadNumber = i;
+            Task.Run(async () =>
+            {
+                Console.WriteLine($"Поток {threadNumber} ожидает доступа к ресурсу.");
+                await semaphore.WaitAsync();
+                try
+                {
+                    Console.WriteLine($"Поток {threadNumber} получил доступ к ресурсу.");
+                    await Task.Delay(1000); // имитация работы с ресурсом
+                }
+                finally
+                {
+                    Console.WriteLine($"Поток {threadNumber} освобождает ресурс.");
+                    semaphore.Release();
+                }
+            });
+        }
+        await Task.Delay(5000);
+    }
+}
